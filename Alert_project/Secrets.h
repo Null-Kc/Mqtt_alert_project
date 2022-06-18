6fbc9d83b4314b37537174543f024bd3351e4409
Pedro Lucas Macedo de Moraes <73931509+Null-pl@users.noreply.github.com>
2022-06-18 16:52:23 -0300

    Add secrets A

---
+++ Alert_project/Secrets.h
@@ -0,0 +1,5 @@
+const char *ssid = ""; // Cria a variavel ssid onde sera armarzenado o nome do WIFI que o ESP se conectara
+const char *password = "";  // Cria a variavel password onde sera armarzenado a sebga do WIFI que o ESP se conectara
+
+const char *mqtt_username = ""; // Cria a variavel mqtt_username onde fica armazenado o login que sera usado para conectar no Broker
+const char *mqtt_password = ""; // Cria a variavel mqtt_password onde fica armazenado a senha que sera usado para conectar no Broker
