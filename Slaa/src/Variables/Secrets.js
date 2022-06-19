d3f8912d9973a990d74bd664723a88c3abdc219c
Pedro Lucas Macedo de Moraes <73931509+Null-pl@users.noreply.github.com>
2022-06-19 03:08:25 -0300

    delete infos

--- Slaa/src/Variables/Secrets.js
+++ Slaa/src/Variables/Secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
-  Mqtt_userName: '441c0bb6@316', // Variavel global username do mqtt
-  Mqtt_password: '8c68e1718b15', // Variavel global senha do mqtt
-  Mqtt_Url: 'mqtt.internetecoisas.com.br' // Variavel global com url do mqtt
+  Mqtt_userName: '', // Variavel global username do mqtt
+  Mqtt_password: '', // Variavel global senha do mqtt
+  Mqtt_Url: '' // Variavel global com url do mqtt
 };
